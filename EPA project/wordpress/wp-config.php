fb77e5b618b1aa3086a1318b80fba827d12f557f
Maphosa, Keith (UK Leeds) <keith.maphosa@baesystems.com>
2024-06-05 15:33:38 +0100

    Added CI/CD pipeline and Dockerfiles, replaced  old files

---
+++ EPA project/wordpress/wp-config.php
@@ -0,0 +1,17 @@
+<?php
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpresspassword' );
+define( 'DB_HOST', 'mysql-service' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
